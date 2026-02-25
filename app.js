533e15a2eabbadb3d68c25c3ab432bbe4f88454e
sonalijagtap401 <sonalijagtap401@gmail.com>
2026-02-25 21:47:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send("Hello devops hi🚀");
+ res.send("Hello devops hi hello🚀");
 });
 
 app.listen(3000, () => {
